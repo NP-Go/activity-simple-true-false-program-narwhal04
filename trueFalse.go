dd3b39da663409cf06d60eed6ac98ba142cb6fcc
narwhal04 <q.quek7@gmail.com>
2022-03-28 17:11:03 +0800

    added if else if and code function

--- trueFalse.go
+++ trueFalse.go
@@ -8,7 +8,13 @@ func compare(value int) string {
 	secretValue := 88
 
 	//Insert your code from here
-
+	if value == secretValue {
+		fmt.Println("Well done! Your guess is correct.")
+	} else if value < secretValue {
+		fmt.Println("Too low, try again next time!")
+	} else if value > secretValue {
+		fmt.Println("Too high, try again next time!")
+	}
 	//do not remove this line
 	return resultMessge
 }
